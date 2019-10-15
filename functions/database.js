eb486e6e24f67bf4c971f2d562bcb69d4bbd17f6
Trollface7272 <49081230+Trollface7272@users.noreply.github.com>
2019-10-15 11:27:16 +0200

    Add files via upload

--- functions/database.js
+++ functions/database.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql')
 module.exports = {
     insert(connection, set, table) {
         let sql = "INSERT INTO " + table + " SET ?"
@@ -6,6 +7,33 @@ module.exports = {
     update(connection, table, id, set) {
         let sql = "UPDATE " + table + " SET ? WHERE discord_id = " + id
         connection.query( sql, set )
+    },
+    handleDisconnect() {
+        var connection = {
+            host     : 'remotemysql.com',
+            user     : 'lWlguk3gRa',
+            password : 'M1rSnLmV6K',
+            database : 'lWlguk3gRa'
+        }
+    
+        connection = mysql.createConnection(connection)
+  
+        connection.connect(function(err) {
+        if(err) {
+            console.log('error when connecting to db:', err)
+            setTimeout(module.exports.handleDisconnect, 2000)
+        }
+        })
+
+        connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            module.exports.handleDisconnect()
+        } else {
+            throw err
+        }
+        })
+        return connection
     }
 
 }
