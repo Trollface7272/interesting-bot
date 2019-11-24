d53679e9108861650ab24bbfb603651307eb75a8
Trollface7272 <trollface7272@post.cz>
2019-11-24 16:37:21 +0100

    419

--- features/ctb.js
+++ features/ctb.js
@@ -78,7 +78,7 @@ module.exports = {
                 if(secondDiff > 0) diffAr[diffAr.length] = secondDiff + ' Seconds '
 
                 //Put 2 diffs into 1 final var
-                let diffFin = diffAr[0] + diffAr[1]
+                let diffFin = diffAr[1] === undefined ? diffAr[0] + diffAr[1] : diffAr[0]
 
                 //Get retries
                 let retries = osuStuff.getAmmountOfRetries(score)
